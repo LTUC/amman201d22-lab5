4ec8c04178cb8e1498c6b8fef6500c583f6ccec8
latifaghassan <lalooshamayra@hotmail.com>
2021-04-08 19:17:57 +0300

    problem05 is done

--- app.js
+++ app.js
@@ -113,11 +113,21 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiplyArray(multArr) {
-  //eslint-disable-line
+
+  let sum2 = multArr[0];
+
+  for ( let i = 1 ; i < multArr.length ; i++){
+
+    sum2 = multiply(sum2,multArr[i])[0];
+
+  }
+
+  return [sum2, `The numbers ${multArr[0]},${multArr[1]},${multArr[2]} have a product of ${sum2}.`] 
+
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop.
 
