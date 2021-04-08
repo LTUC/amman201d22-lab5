bea8b9dbd5a694ead65d0731eb068568c5ee36a2
Qusai-Alnonah <kusaialnona@yahoo.com>
2021-04-08 17:21:31 +0300

    solve first prblem

--- app.js
+++ app.js
@@ -8,8 +8,9 @@ Write a function called sum() that takes in two numbers as arguments and then re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
 // Write your code here
-function sum(a, b) {
-  //eslint-disable-line
+function sum(a,b) {
+  let x = a + b 
+  return [x , 'The sum of ' + a + ' and ' + b + ' is ' + x +'.'];
 }
 // Here is the test for sum(); uncomment it to run it
 testSum(4, 7);
