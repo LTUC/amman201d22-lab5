f2ec8e31c6a6b3872fa936f07a2343d214a4c594
ossamaaa <osskhsh@gmail.com>
2021-04-08 18:26:21 +0300

    added prob3

--- app.js
+++ app.js
@@ -50,12 +50,15 @@ return[z,`The product of ${a} and ${b} is ${z}.`]
 // Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumAndMultiply() function and see if the test passes.*/
 
 // // Write your code here
-// function sumAndMultiply(a, b, c) {
-//   //eslint-disable-line
-// }
+function sumAndMultiply(a, b, c) {
 
-// // Here is the test for sumAndMultiply(); uncomment it to run it
-// // testSumAndMultiply(4,7,5);
+let sumation = sum(sum(a,b)[0],c)[0];
+let multiplication = multiply(multiply(a,b)[0],c)[0];
+return [sumation,multiplication,`${a} and ${b} and ${c} sum to ${sumation}.`,`The product of ${a} and ${b} and ${c} is ${multiplication}.`]
+}
+
+// Here is the test for sumAndMultiply(); uncomment it to run it
+testSumAndMultiply(4,7,5);
 
 // // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -69,16 +72,16 @@ return[z,`The product of ${a} and ${b} is ${z}.`]
 
 // Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumArray() function and see if the test passes.*/
 
-// // Write your code here
+// // // Write your code here
 // let testArray = [2, 3, 4]; //eslint-disable-line
 
 // function sumArray(sumArr) {
-//   //eslint-disable-line
+// let = sum
 // }
 
-// // Here is the test for sumArray(); uncomment it to run it
+// // // Here is the test for sumArray(); uncomment it to run it
 
-// // testSumArray(testArray);
+// testSumArray(testArray);
 
 // // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
