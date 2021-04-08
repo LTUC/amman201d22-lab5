8d4bcc00c2b134dd37b083f646beb85504e6d9d9
Anas Al-Ramahi <anas_alramahi@outlook.com>
2021-04-08 17:25:52 +0300

    added the content of the stretch goal

--- app.js
+++ app.js
@@ -123,24 +123,40 @@ testMultiplyArray(testArray);
 
 /////////////////////////////////////
 /* STRETCH GOAL: Problem 6
-Write a function called multiplyAnyArray() that takes an array of numbers of any length as its argument and returns an array whose first element is the product of those numbers, and the second element is a string that EXACTLY follows this example and concatenates a message using the arguments that were passed into the function:
+Write a function called multiplyAnyArray() that takes an array of numbers of any length as its argument and returns an array whose
+ first element is the product of those numbers, and the second element is a string that EXACTLY follows this example and concatenates 
+ a message using the arguments that were passed into the function:
 
 "The numbers 1,2,3,4,5 have a product of 120."
 
-IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To do multiplication, use your multiply() function that you've already created. You're going to have to be resourceful to figure out how to do this. However, you may continue to use the + operator for string concatenation.
+IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To do multiplication, use your multiply() function 
+that you've already created. You're going to have to be resourceful to figure out how to do this. However, you may continue to use 
+the + operator for string concatenation.
 
 This function should be dynamic, accepting an array of any length.
 
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiplyAnyArray() function and see if the test passes.*/
 
 // Write your code here
-let testDynamicArray = [1, 2, 3, 4, 5]; //eslint-disable-line
+let testDynamicArray = [1, 2, 3, 4, 5]; 
+//eslint-disable-line
 
 function multiplyAnyArray(dynamicArray) {
   //eslint-disable-line
+  let multplicatoinStored = 1;
+  let concatinationStored = '';
+  let arrayCopy = [...dynamicArray];
+
+  for(let j=0; j<arrayCopy.length-1; j++){
+    multplicatoinStored = multiply(arrayCopy[j],arrayCopy[j+1])[0];
+    arrayCopy[j+1] = multplicatoinStored;
+    concatinationStored += dynamicArray[j] + ',';
+  }
+  multplicatoinStored = arrayCopy[arrayCopy.length-1];
+  concatinationStored += dynamicArray[dynamicArray.length-1];
+
+  return [multplicatoinStored, `The numbers ${concatinationStored} have a product of ${multplicatoinStored}.`];
 }
-
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyAnyArray(testDynamicArray);
-
+testMultiplyAnyArray(testDynamicArray);
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. You're done! Submit the link to the repo following the instructions in Canvas.
