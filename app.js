24ab728e92e40220f655a46b20bb65c9d76abb5d
mohammad ahmad theeb hayajneh <mohammadhayagneh96@gmail.com>
2021-04-08 15:27:38 +0300

    p4

--- app.js
+++ app.js
@@ -50,8 +50,10 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) {
-  let x =(a+b+c);
-  let y =(a*b*c);
+ let w= sum(a, b)[0];
+ let x = sum(w, c)[0];
+ let q= multiply(a,b)[0];
+  let y =multiply(q,c)[0];
   return[x,y,+ a +' and '+ b +' and '+ c +' sum to '+ x +'.','The product of '+a+' and '+ b +' and '+c+' is '+ y +'.'];
 
 
@@ -76,12 +78,14 @@ Test this function by hand in the console to get it working, and when you think 
 let testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) {
-  //eslint-disable-line
+  let x = sum(sumArr[0], sumArr[1])[0];
+  let y = sum(x, sumArr[2])[0];
+  return[ y,sumArr[0]+','+sumArr[1]+','+sumArr[2]+' was passed in as an array of numbers, and '+y+' is their sum.'];
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+ testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
