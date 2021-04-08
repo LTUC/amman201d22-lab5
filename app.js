de1ff17b016af2f94db6888aeb90576a1b23962d
ossamaaa <osskhsh@gmail.com>
2021-04-08 15:26:29 +0300

    added prob2

--- app.js
+++ app.js
@@ -27,12 +27,14 @@ testSum(4, 7);
 // Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
 // // Write your code here
-// // function multiply(a, b) {
-//   //eslint-disable-line
-// // }
+ function multiply(a, b) {
+let z =a*b
+return[z,`The product of ${a} and ${b} is ${z}.`]
+
+}
 
 // // Here is the test for multiply(); uncomment it to run it
-// // testMultiply(5,9);
+ testMultiply(5,9);
 
 // // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
