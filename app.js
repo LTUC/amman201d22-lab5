b512d51e9745ed987023eedd3e33ea52a69f2f0c
Jana Abdelkader <janaosama1300@gmail.com>
2021-04-08 15:16:47 +0300

    Questions 1 & 2

--- app.js
+++ app.js
@@ -9,7 +9,8 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sum(a, b) {
-  //eslint-disable-line
+  let answer = a+b; 
+  return [answer, `The sum of ${a} and ${b} is ${answer}.`];
 }
 // Here is the test for sum(); uncomment it to run it
 testSum(4, 7);
@@ -25,12 +26,13 @@ Write a function called multiply() that takes in two numbers as arguments and re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
 // Write your code here
-function multiply(a, b) {
-  //eslint-disable-line
+function multiply(a, b){
+  let answer = a*b; 
+  return [answer, `The product of ${a} and ${b} is ${answer}.`];
 }
-
+//console.log(multiply(3,6));
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
