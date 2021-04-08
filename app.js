2addc9c928410967986ddd06e09e8f0a80deb099
Saif Momani <saif.aq.momani@gmail.com>
2021-04-08 15:35:47 +0300

    func4

--- app.js
+++ app.js
@@ -80,11 +80,10 @@ let testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) {
   //eslint-disable-line
-  let a = 0;
-  testArray.forEach((e) => {
-    a += e;
-  });
-  return [a, `2,3,4 was passed in as an array of numbers, and 9 is their sum.`];
+  return [
+    sum(sum(sumArr[0], sumArr[1])[0], sumArr[2])[0],
+    `2,3,4 was passed in as an array of numbers, and 9 is their sum.`,
+  ];
 }
 
 // Here is the test for sumArray(); uncomment it to run it
@@ -106,10 +105,14 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function multiplyArray(multArr) {
   //eslint-disable-line
+  return [
+    multiply(multiply(multArr[0], multArr[1])[0], multArr[2])[0],
+    `The numbers 2,3,4 have a product of 24`,
+  ];
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop.
 
