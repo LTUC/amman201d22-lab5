1e57f0223311141cd5785181790a12cd419759e2
Qusai-Alnonah <kusaialnona@yahoo.com>
2021-04-08 22:49:25 +0300

    Solving Problem2

--- app.js
+++ app.js
@@ -8,12 +8,12 @@ Write a function called sum() that takes in two numbers as arguments and then re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
 // Write your code here
-// function sum(a,b) {
-//   let x = a + b 
-//   return [x , 'The sum of ' + a + ' and ' + b + ' is ' + x +'.'];
-// }
+ function sum(a,b) {
+   let x = a + b 
+   return [x , 'The sum of ' + a + ' and ' + b + ' is ' + x +'.'];
+ }
 // // Here is the test for sum(); uncomment it to run it
-// testSum(4, 7);
+ testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -23,15 +23,17 @@ Write a function called multiply() that takes in two numbers as arguments and re
 
 "The product of 5 and 9 is 45."
 
-Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
+Test thuncomment the call for the testMultiply() function and is function by hand in the console to get it working, and when you think it is finished, see if the test passes.*/
 
 // Write your code here
 function multiply(a, b) {
+  let z = a * b ;
+  return[z , 'The product of ' + a +  ' and ' + b + ' is ' + z +'.'];
   
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
