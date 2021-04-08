689d27138e8e1ed01fb043069448c457153f1ae2
Roaa Mustafa <68363882+RoaaMustafa@users.noreply.github.com>
2021-04-08 18:25:59 +0300

    Revert "problem 5 is done"

--- app.js
+++ app.js
@@ -104,17 +104,11 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiplyArray(multArr) {
-  let multiplication = 1;
-  for (let i= 0; i < multArr.length; i++) {
-    multiplication = multiply(multiplication,multArr[i])[0];
-  }
- return [multiplication,`The numbers ${multArr[0]},${multArr[1]},${multArr[2]} have a product of ${multiplication}.`];
- 
-
+  //eslint-disable-line
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
- testMultiplyArray(testArray);
+// testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop.
 
