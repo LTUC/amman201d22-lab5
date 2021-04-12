a71a8db5f9d32a94eeb5c733eee39e24bcb60ac4
Abelmajeed Alabedalraheem <abd.d@live.com>
2021-04-12 10:10:07 +0300

    e

--- app.js
+++ app.js
@@ -90,14 +90,16 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiplyArray(multArr) {
-multArr = testArray
-let z = multArr[0]*multArr[1]*multArr[2]
-return [z,'The numbers '+multArr[0]+','+ multArr[1] +','+ multArr[2] +' have a product of '+z+'.']
+  //eslint-disable-line
+  let x=1;
+  for(let i =0;i<multArr.length ; i++){
+    x=multiply(x,multArr[i])[0];
+  }
+  return[x,`The numbers ${multArr} have a product of ${x}.`]
 }
 
-
 // Here is the test for multiplyArray(); uncomment it to run it
- testMultiplyArray(testArray);
+testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop.
 
